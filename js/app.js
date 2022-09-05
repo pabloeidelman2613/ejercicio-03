fed592319d14104c66c517916088ed9b19e3420d
pabloeidelman <pabloeidelman26@gmail.com>
2022-09-05 18:06:39 -0300

    Agrego prompt para la cantidad de veces que se repite el video

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ const myDoc = document;
 
 let myLi = myDoc.getElementById("elemento");
 
-const cant_elements = 8;
+const cant_elements = prompt("Ingrese la cantidad de veces que se va a " +
+"repetir el video");
 
 const elemento = myLi.innerHTML; 
 
-for (let index = 0; index < cant_elements; index++) {
+for (let index = 1; index < cant_elements; index++) {
       myLi.innerHTML += elemento;
 }
